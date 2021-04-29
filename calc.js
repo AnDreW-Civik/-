b801d9a97e324c8e2dd28265ac68e01af3d8244f
AnDreW-Civik <83407763+AnDreW-Civik@users.noreply.github.com>
2021-04-29 15:54:02 +0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,23 +1,4 @@
 
-// Рекурсия;
-// Реализуйте (с использованием рекурсивного процесса) функцию sequenceSum, которая находит
-// сумму последовательности целых чисел.
-// Последовательность задается двумя значениями: begin - начало последовательности, end - конец последовательности.
-// Например: begin = 2 и end = 6 дают нам такую последовательность 2, 3, 4, 5, 6. Сумма такой
-// последовательности будет: 20.
-//
-// const sequenceSum = (begin, end) => {
-//     if (begin > end) {
-//         return NaN;
-//     } if (begin === end) {
-//         return begin;
-//     }
-//     return end + sequenceSum(begin, end - 1);
-// };
-
-// }
-
-
     function calc () {
            var tabl1 = document.getElementById("chislo");
             var tabl2 = document.getElementById("chislo1");
