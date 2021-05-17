93bc475830f5ac922bd1075c5873c61fa47d9684
AnDreW-Civik <83407763+AnDreW-Civik@users.noreply.github.com>
2021-05-17 15:29:31 +0300

    Add files via upload

--- calc.js
+++ calc.js
@@ -1,7 +1,8 @@
 var x;
+
     function calc () {
-        var tabl1 = document.getElementById("chislo");
-        var tabl2 = document.getElementById("chislo1");
+        var tabl1 = document.getElementById("number");
+        var tabl2 = document.getElementById("number1");
 
         var res1 = parseFloat(tabl1.value);
         var res2 = parseFloat(tabl2.value);
@@ -10,8 +11,8 @@ var x;
         };
 
         function calc1() {
-            var tabl1 = document.getElementById("chislo");
-            var tabl2 = document.getElementById("chislo1");
+            var tabl1 = document.getElementById("number");
+            var tabl2 = document.getElementById("number1");
 
             var res1 = parseFloat(tabl1.value);
             var res2 = parseFloat(tabl2.value);
@@ -21,19 +22,20 @@ var x;
         };
 
 function calc2() {
-    var tabl1 = document.getElementById("chislo");
-    var tabl2 = document.getElementById("chislo1");
+    var tabl1 = document.getElementById("number");
+    var tabl2 = document.getElementById("number1");
 
     var res1 = parseFloat(tabl1.value);
     var res2 = parseFloat(tabl2.value);
 
     x = res1 + res2;
+
     return x;
 };
 
 function calc3() {
-    var tabl1 = document.getElementById("chislo");
-    var tabl2 = document.getElementById("chislo1");
+    var tabl1 = document.getElementById("number");
+    var tabl2 = document.getElementById("number1");
 
     var res1 = parseFloat(tabl1.value);
     var res2 = parseFloat(tabl2.value);
@@ -43,7 +45,7 @@ function calc3() {
 };
 
 
-function ravno () {
+function equally () {
     result.innerHTML = x;
 };
 
